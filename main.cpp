22b588734330174c3c7e0b650fe8f78d3dfa2417
Marc Jacques <39604945+MarcJacques@users.noreply.github.com>
2023-02-15 21:51:53 +0000

    adds random numbers to lottery adds array comparison to see if you won the lottery

--- main.cpp
+++ main.cpp
@@ -7,5 +7,24 @@ int main() {
   int lottery[LENGTH];
   int user[LENGTH];
 
+  for (int i = 0; i < LENGTH; i++)
+    {
+      cin >> user[i];
+  //generate lottery array
+    for (int j = 0; j < LENGTH; j++)
+      lottery[i] = rand() % 100 + 1;
+  
+    }
+  int match = 0;
+  for (int i = 0; i < LENGTH; i++)
+    {
+      if (user[i] == lottery[i])
+        match++;
+      
+    }
+  if (match == 5)
+        cout << "Winner";
+      else
+        cout << "Sorry";
   
 }
